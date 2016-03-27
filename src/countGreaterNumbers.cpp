4c24d574ced7f1521fa47b18f0293a59a8a66449
Nikhil-a1 <Nikhil-a1@missionrnd.com>
2016-03-28 01:12:03 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,6 +13,8 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
+//time complexity O(n)
+#include<stdlib.h>
 
 struct transaction {
 	int amount;
@@ -20,6 +22,102 @@ struct transaction {
 	char description[20];
 };
 
+int check_valid_date(struct calender*);//to check whether input is valid or not
+int search_for_transaction(struct calender*, int, struct calender*);
+
+struct calender
+{
+	int dat;
+	int mnth;
+	int yr;
+};
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	if (Arr == NULL || len <= 0 || date == NULL)
+		return -1;
+	int i = 0, j = 0, flag = 1, trasctn_after_given_date;
+	struct calender *calen;
+	calen = (struct calender*)calloc(sizeof(struct calender), len);
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; Arr[i].date[j] != '-'; j++)
+			calen[i].dat = calen[i].dat * 10 + (Arr[i].date[j] - '0');
+		j++;
+		for (; Arr[i].date[j] != '-'; j++)
+			calen[i].mnth = calen[i].mnth * 10 + (Arr[i].date[j] - '0');
+		j++;
+		for (; Arr[i].date[j] != '\0'; j++)
+			calen[i].yr = calen[i].yr * 10 + (Arr[i].date[j] - '0');
+	}
+	struct calender *input;
+	input = (struct calender*)calloc(sizeof(struct calender), 1);
+	for (; *date != '-'; date++)
+		input->dat = input->dat * 10 + (*date - '0');
+	date++;
+	for (; *date != '-'; date++)
+		input->mnth = input->mnth * 10 + (*date - '0');
+	date++;
+	for (; *date != '\0'; date++)
+		input->yr = input->yr * 10 + (*date - '0');
+	flag = check_valid_date(input);
+	if (flag == 0)
+		return -1;
+	else
+		trasctn_after_given_date = search_for_transaction(calen, len, input);
+	return trasctn_after_given_date;
+}
+int check_valid_date(struct calender* input)
+{
+	int flag = 0;
+	if (input->yr <= 0)
+		return flag;
+	else if (input->mnth > 12 || input->mnth <= 0)
+		return flag;
+	else if (input->dat > 31 || input->dat <= 0)
+		return flag;
+	else
+	{
+		switch (input->mnth)
+		{
+		case 2: if (input->yr % 100 == 0 && input->yr % 400 == 0 && input->dat > 29)
+			return flag;
+				else if (input->yr % 4 == 0 && input->dat > 29)
+					return flag;
+				else if (input->dat > 28)
+					return flag;
+				break;
+		case 4:
+		case 6:
+		case 8:
+		case 9:
+		case 11: if (input->dat > 30)
+			return flag;
+			break;
+		default:return flag = 1;
+			break;
+		}
+	}
+}
+
+int search_for_transaction(struct calender* calen, int len, struct calender* input)
+{
+	int transaction = 0, i = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (calen[i].yr>input->yr)
+		{
+			transaction = len - i ;
+			break;
+		}
+		else if (calen[i].mnth > input->mnth && calen[i].yr >= input->yr)
+		{
+			transaction = len - i ;
+			break;
+		}
+		else if (calen[i].dat > input->dat && calen[i].mnth >= input->mnth && calen[i].yr >= input->yr)
+		{
+			transaction = len - i ;
+			break;
+		}
+	}
+	return transaction;
 }
